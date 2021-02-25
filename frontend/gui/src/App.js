f2e7f3dfb302655e861bc4883ea0e04910d0b0cd
omar115 <omarhasan115@gmail.com>
2021-02-26 03:23:47 +0600

    layout added, backend api cration

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import CustomLayout from './containers/Layout'
+import ArticleList from './containers/ArticleListView'
+
+function App() {
+  return (
+    <div className="App">
+      <CustomLayout>
+        <ArticleList />
+      </CustomLayout>
+    </div>
+  );
+}
+
+export default App;
